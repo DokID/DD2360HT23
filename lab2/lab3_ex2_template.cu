bfe243e6fece24181c72fa35c4b003183c9e7d8f
Gábor Nagy <gaborn@kth.se>
2023-11-22 20:28:05 +0100

    Add code for basic instruction sequence
    Plus copy some boilerplate from ex1, and determine correct number
    of threads and blocks.
    
    Co-authored-by: Christian Weigelt <weigelt@kth.se>

--- lab2/lab3_ex2_template.cu
+++ lab2/lab3_ex2_template.cu
@@ -1,6 +1,8 @@
-
 #include <stdio.h>
 #include <sys/time.h>
+#include <random>
+#include <string>
+#include <math.h>
 
 #define DataType double
 
@@ -9,6 +11,28 @@ __global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
                       int numAColumns, int numBRows, int numBColumns){
   //@@ Insert code to implement matrix multiplication here
 
+struct timeval* startTime = (timeval *) malloc(sizeof(timeval));
+
+//@@ Insert code to implement timer start
+
+void starTimer() {
+  gettimeofday(startTime, NULL);
+}
+
+//@@ Insert code to implement timer stop
+
+void stopTimer() {
+  struct timeval* currentTime = (timeval *) malloc(sizeof(timeval));
+  struct timeval* diff = (timeval *) malloc(sizeof(timeval));
+
+  gettimeofday(currentTime, NULL);
+  timersub(currentTime, startTime, diff);
+
+  printf("Time in seconds: %ld\n", diff->tv_sec);
+  printf("Time in microseconds: %ld\n", diff->tv_usec);
+
+  free(currentTime);
+  free(diff);
 }
 
 int main(int argc, char **argv) {
@@ -27,55 +51,99 @@ int main(int argc, char **argv) {
   int numCRows;
   int numCColumns;
 
-  //@@ Insert code below to read in numARows, numAColumns, 
+  //@@ Insert code below to read in numARows, numAColumns,
   numARows = std::stoi(argv[1], nullptr);   
   numAColumns = std::stoi(argv[2], nullptr);
   numBRows = std::stoi(argv[3], nullptr);   
   numBColumns = std::stoi(argv[4], nullptr);
-  numCRows = std::stoi(argv[5], nullptr);
-  numCColumns = std::stoi(argv[6], nullptr);
+  numCRows = numARows;
+  numCColumns = numBColumns;
 
   printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
 
   // check dims match
-  if !((numARows == numCRows) && (numBColumns == numCColumns) && (numAColumns == numBRows)) {
+  if (numAColumns != numBRows) {
     fprintf(stderr, "Input dim mismatch.\n");
     return -1;
   }
-  
+
   //@@ Insert code below to allocate Host memory for input and output
   // index by offset = i + numAColumns * j
-  DataType *hostA = (DataType *) malloc(numARows * numAColumns * sizeof(DataType));
-  // index by 
-  DataType **hostA = (DataType **) malloc(numARows * sizeof(DataType *));
-  for(int i = 0; i < numARows; i++) hostA[i] = (DataType *)malloc(numAColumns * sizeof(DataType));
-  
-  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  hostA = (DataType *) malloc(numARows * numAColumns * sizeof(DataType));
+  hostB = (DataType *) malloc(numBRows * numBColumns * sizeof(DataType));
+  hostC = (DataType *) malloc(numCRows * numCColumns * sizeof(DataType));
+  resultRef = (DataType *) malloc(numCRows * numCColumns * sizeof(DataType));
 
+
+  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  for (int i = 0; i < numARows; i++) {
+    for (int j = 0; j < numAColumns; j++) {
+      hostA[i * numAColumns + j] = ((DataType)rand() / RAND_MAX) * (DataType) 10;
+    }
+  }
+  for (int i = 0; i < numBRows; i++) {
+    for (int j = 0; j < numBColumns; j++) {
+      hostB[i * numBColumns + j] = ((DataType)rand() / RAND_MAX) * (DataType) 10;
+    }
+  }
+
+  printf("\nRunning on CPU...\n");
+  starTimer();
+  gemmCPU(hostA, hostB, resultRef, numARows, numAColumns, numBRows, numBColumns);
+  stopTimer();
 
   //@@ Insert code below to allocate GPU memory here
+  cudaMalloc(&deviceA, numARows * numAColumns * sizeof(DataType));
+  cudaMalloc(&deviceB, numBRows * numBColumns * sizeof(DataType));
+  cudaMalloc(&deviceC, numCRows * numCColumns * sizeof(DataType));
 
+  printf("Copying memory to device...\n");
 
   //@@ Insert code to below to Copy memory to the GPU here
-
+  starTimer();
+  cudaMemcpy(deviceA, hostA, numARows * numAColumns * sizeof(DataType), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceB, hostB, numBRows * numBColumns * sizeof(DataType), cudaMemcpyHostToDevice);
+  stopTimer();
 
   //@@ Initialize the grid and block dimensions here
+  // cudaDeviceProp *prop = (cudaDeviceProp *) malloc (sizeof(cudaDeviceProp));
+  // cudaGetDeviceProperties_v2(prop, 0);
+  // int threads_per_block = prop->maxThreadsPerBlock;
+  // int no_of_blocks = (int) ceil(double(inputLength) / double(threads_per_block));
 
+  dim3 block(32, 32);
+  dim3 grid(ceil((double) numARows / (double) 32), ceil((double) numBColumns / (double) 32));
+
+  printf("\nRunning kernel...\n");
 
   //@@ Launch the GPU Kernel here
+  starTimer();
+  gemm<<<grid, block>>>(deviceA, deviceB, deviceC, numARows, numAColumns, numBRows, numBColumns);
+  cudaDeviceSynchronize();
+  stopTimer();
 
+  printf("\nCopying results to host...\n");
 
   //@@ Copy the GPU memory back to the CPU here
-
+  starTimer();
+  cudaMemcpy(hostC, deviceC, numCRows * numCColumns * sizeof(DataType), cudaMemcpyDeviceToHost);
+  stopTimer();
 
   //@@ Insert code below to compare the output with the reference
 
+  printf("\nResults match: %s\n", calculateDiff(resultRef, hostC, numCRows, numCColumns) ? "false" : "true");
 
   //@@ Free the GPU memory here
-
+  cudaFree(deviceA);
+  cudaFree(deviceB);
+  cudaFree(deviceC);
 
   //@@ Free the CPU memory here
-
+  free(hostA);
+  free(hostB);
+  free(hostC);
+  free(resultRef);
+  free(startTime);
 
   return 0;
 }
