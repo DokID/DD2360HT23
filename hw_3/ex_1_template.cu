9de01c30650db555d01dee1313d119e79509f025
Gábor Nagy <gaborn@kth.se>
2023-12-04 21:34:56 +0100

    Add initial solution to HW3 Ex1
    Co-authored-by: Christian Weigelt <weigelt@kth.se>

--- hw_3/ex_1_template.cu
+++ hw_3/ex_1_template.cu
@@ -1,81 +1,138 @@
 
 #include <stdio.h>
 #include <sys/time.h>
+#include <string>
 #include <random>
+#include <math.h>
 
 #define NUM_BINS 4096
+#define MAX_INPUT_LENGTH 1 << 32;
+#define MAX_SATURATION 127
+#define DataType unsigned int
 
-__global__ void histogram_kernel(unsigned int *input, unsigned int *bins,
-                                 unsigned int num_elements,
-                                 unsigned int num_bins) {
+__global__ void histogram_kernel(DataType *input, DataType *bins, DataType num_elements, DataType num_bins) {
+
+  const int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  if (idx >= num_elements) return;
 
 //@@ Insert code below to compute histogram of input using shared memory and atomics
+  __shared__ DataType s_bins[NUM_BINS];
+  atomicAdd(&s_bins[idx], 1);
+  __syncthreads();
 
-
+  if (idx == 0) {
+    for (int i = 0; i < num_bins; i++) {
+      atomicAdd(&bins[i], s_bins[i]);
+    }
+  }
 }
 
-__global__ void convert_kernel(unsigned int *bins, unsigned int num_bins) {
+
+__global__ void convert_kernel(DataType *bins, DataType num_bins) {
+
+  const int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  if (idx >= num_bins) return;
 
 //@@ Insert code below to clean up bins that saturate at 127
+  if(bins[idx] > MAX_SATURATION) bins[idx] = MAX_SATURATION; // set value of saturated bin to MAX_SATURATION
+}
 
 
+void histogram_CPU(DataType *input, DataType *bins, DataType num_elements, DataType num_bins) {
+  for (int i = 0; i < num_elements; i++) {
+    bins[input[i]] += 1;
+  }
+
+  for (int i = 0; i < num_bins; i++) {
+    if (bins[i] > MAX_SATURATION) bins[i] = MAX_SATURATION;
+  }
+}
+
+
+int calculateDiff(DataType *host_bins, DataType *device_bins, DataType num_bins) {
+  int flag = 0;
+  for (int i = 0; i < num_bins; i++) {
+    if (host_bins[i] != device_bins[i]) {
+      printf("Results differ at %d: Host: %lu; Device: %lu", i, host_bins[i], device_bins[i]);
+      flag = 1;
+    }
+  }
+
+  return flag;
 }
 
 
 int main(int argc, char **argv) {
   
   int inputLength;
-  unsigned int *hostInput;
-  unsigned int *hostBins;
-  unsigned int *resultRef;
-  unsigned int *deviceInput;
-  unsigned int *deviceBins;
+  DataType *hostInput;
+  DataType *hostBins;
+  DataType *resultRef;
+  DataType *deviceInput;
+  DataType *deviceBins;
 
   //@@ Insert code below to read in inputLength from args
+  inputLength = std::stoi(argv[1], nullptr);
 
-  printf("The input length is %d\n", inputLength);
+  //printf("The input length is %d\n", inputLength);
   
   //@@ Insert code below to allocate Host memory for input and output
-
+  hostInput = (DataType*) malloc(inputLength * sizeof(DataType));
+  hostBins = (DataType*) malloc(NUM_BINS * sizeof(DataType));
+  resultRef = (DataType*) malloc(NUM_BINS * sizeof(DataType));
   
   //@@ Insert code below to initialize hostInput to random numbers whose values range from 0 to (NUM_BINS - 1)
-
+  for (int i = 0; i < inputLength; i++) {
+    hostInput[i] = (DataType)floor(((double)rand() / (double)RAND_MAX) * (double)(NUM_BINS - 1));
+  }
 
   //@@ Insert code below to create reference result in CPU
-
+  histogram_CPU(hostInput, resultRef, inputLength, NUM_BINS);
 
   //@@ Insert code below to allocate GPU memory here
-
+  cudaMalloc(&deviceInput, inputLength * sizeof(DataType));
+  cudaMalloc(&deviceBins, NUM_BINS * sizeof(DataType));
 
   //@@ Insert code to Copy memory to the GPU here
-
+  cudaMemcpy(deviceInput, hostInput, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
 
   //@@ Insert code to initialize GPU results
-
+  cudaMemset(deviceBins, 0, NUM_BINS);
 
   //@@ Initialize the grid and block dimensions here
-
+  cudaDeviceProp *prop = (cudaDeviceProp *) malloc (sizeof(cudaDeviceProp));
+  cudaGetDeviceProperties_v2(prop, 0);
+  int threads_per_block = prop->maxThreadsPerBlock;
+  
+  dim3 block1(threads_per_block);
+  dim3 grid1((int)ceil((double)inputLength/(double)block1.x));
 
   //@@ Launch the GPU Kernel here
-
+  histogram_kernel<<<grid1, block1>>>(deviceInput, deviceBins, inputLength, NUM_BINS);
+  cudaDeviceSynchronize();
 
   //@@ Initialize the second grid and block dimensions here
-
+  dim3 block2(threads_per_block);
+  dim3 grid2((int)ceil((double)NUM_BINS/(double)block2.x));
 
   //@@ Launch the second GPU Kernel here
-
+  convert_kernel<<<grid2, block2>>>(deviceBins, NUM_BINS);
+  cudaDeviceSynchronize();
 
   //@@ Copy the GPU memory back to the CPU here
-
+  cudaMemcpy(hostBins, deviceBins, NUM_BINS * sizeof(DataType), cudaMemcpyDeviceToHost);
 
   //@@ Insert code below to compare the output with the reference
-
+  printf("\n Results match: %s\n", calculateDiff(resultRef, hostBins, NUM_BINS) ? "false" : "true");
 
   //@@ Free the GPU memory here
-
+  cudaFree(deviceInput);
+  cudaFree(deviceBins);
 
   //@@ Free the CPU memory here
-
+  free(hostInput);
+  free(hostBins);
+  free(resultRef);
 
   return 0;
 }
